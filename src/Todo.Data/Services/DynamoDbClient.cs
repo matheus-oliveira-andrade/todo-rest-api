c8268465e9b15f59960879c05fc986282758fbbe
Matheus de Oliveira <32457879+micrommath@users.noreply.github.com>
2022-05-20 21:54:02 -0300

    Feat/aws lambda project (#5)
    * feat add lambda project
    
    * fix dynamodb client credentials

--- src/Todo.Data/Services/DynamoDbClient.cs
+++ src/Todo.Data/Services/DynamoDbClient.cs
@@ -1,28 +1,14 @@
-﻿using Amazon;
-using Amazon.DynamoDBv2;
-using Amazon.Runtime;
-using Amazon.Runtime.CredentialManagement;
+﻿using Amazon.DynamoDBv2;
 
 namespace Todo.Data.Services
 {
     public class DynamoDbClient
     {
         public readonly IAmazonDynamoDB DataBase;
-        private readonly RegionEndpoint _defaultRegionEndpoint = RegionEndpoint.USEast1;
-        
-        public DynamoDbClient(string profileName = "default", RegionEndpoint regionEndpoint = null)
+
+        public DynamoDbClient()
         {
-            regionEndpoint ??= _defaultRegionEndpoint;
-
-            DataBase = new AmazonDynamoDBClient(GetLocalStoredCredentials(profileName), regionEndpoint);
-        }
-
-        private static AWSCredentials GetLocalStoredCredentials(string profileName)
-        {
-            var chain = new CredentialProfileStoreChain();
-            var success = chain.TryGetAWSCredentials(profileName, out var awsCredentials);
-
-            return awsCredentials;
+            DataBase = new AmazonDynamoDBClient();
         }
     }
 }
